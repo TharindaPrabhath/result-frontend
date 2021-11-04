2cb2487126c200fd81e7205940d337acce7c146f
tharinda <tharindahp@gmail.com>
2021-11-05 00:39:33 +0530

    fix components wrapping up issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import "../styles/globals.css";
+
+// next
 import type { AppProps } from "next/app";
+import type { NextPage } from "next";
+
+// mui
+import { Button, ThemeProvider } from "@mui/material";
+
+import Layout from "../components/Layout";
+import theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
