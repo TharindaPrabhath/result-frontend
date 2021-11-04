7d958fd53f7cdb92821a360e32b9b47bc8f6234e
tharinda <tharindahp@gmail.com>
2021-11-04 16:24:11 +0530

    set up the layout

---
+++ constants/index.ts
@@ -0,0 +1,9 @@
+export const BRAND = {
+  fullName: "Sasnaka Sansada Foundation",
+  shortName: "Sasnaka",
+  logo: {
+    logo1:
+      "https://learnsteer.sasnaka.org/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2020/12/sasnaka-learn-steer.png.webp",
+    logo2: "",
+  },
+};
---
+++ theme/index.ts
@@ -0,0 +1,28 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0098DA",
+      light: "#E2E2E2",
+    },
+    secondary: {
+      main: "#232630",
+      light: "#FFFFFF",
+    },
+  },
+  typography: {
+    fontFamily: "Poppins",
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+        },
+      },
+    },
+  },
+});
+
+export default theme;
