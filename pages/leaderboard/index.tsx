a4b5be0caf0f253bf2258ecea5d6525af460b4a4
tharinda <tharindahp@gmail.com>
2021-11-07 14:04:22 +0530

    -

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -2,12 +2,19 @@
 import { Typography, Box, Theme, Card } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
+// next
+import { GetServerSideProps } from "next";
+
 // components
 import LeaderboardRow from "../../components/LeaderboardRow";
 
 // constants
 import { TEST_NAME } from "../../constants";
 import { LEADERBOARD_DUMMY_DATA } from "../../constants/dummyData";
+
+// firebase
+import { collection, query, getDocs, orderBy } from "firebase/firestore";
+import { db } from "../../firebase";
 
 const useStyles = makeStyles((theme: Theme) => ({
   box: { backgroundColor: theme.palette.primary.light, padding: "2em 0" },
@@ -32,7 +39,8 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
-export default function Leaderboard() {
+export default function Leaderboard({ props }: { props: any }) {
+  console.log(props);
   const classes = useStyles();
   return (
     <Box className={classes.box}>
@@ -55,3 +63,15 @@ export default function Leaderboard() {
     </Box>
   );
 }
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const q = query(collection(db, "examines"), orderBy("rank"));
+//   const d = await getDocs(q);
+//   let out: any[] = [];
+//   d.docs.forEach((doc) => {
+//     out.push(doc.data());
+//   });
+//   return {
+//     props: out,
+//   };
+// };
