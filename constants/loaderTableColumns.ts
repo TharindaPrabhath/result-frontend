ce3fce91b2a9d3641b2519f8c3cc816386b8f81b
tharinda <tharindahp@gmail.com>
2021-12-09 00:37:23 +0530

    reconfig

--- constants/loaderTableColumns.ts
+++ constants/loaderTableColumns.ts
@@ -1,6 +1,6 @@
 import { GridColDef } from "@mui/x-data-grid";
 
-const nonCombinedMathematics: GridColDef[] = [
+const nonMathematics: GridColDef[] = [
   { field: "id", headerName: "Id", width: 20 },
   { field: "index", headerName: "Index", width: 120 },
   {
@@ -95,7 +95,7 @@ const nonCombinedMathematics: GridColDef[] = [
   },
 ];
 
-const combinedMathematics: GridColDef[] = [
+const mathematics: GridColDef[] = [
   { field: "id", headerName: "Id", width: 20 },
   { field: "index", headerName: "Index", width: 120 },
   {
@@ -111,139 +111,127 @@ const combinedMathematics: GridColDef[] = [
     editable: false,
   },
   {
-    field: "pureTotalMarks",
-    headerName: "Pure Total Marks",
+    field: "paperTotalMarks",
+    headerName: "Paper Total Marks",
     width: 180,
     editable: false,
   },
   {
-    field: "appliedTotalMarks",
-    headerName: "Applied Total Marks",
-    width: 180,
+    field: "structured",
+    headerName: "Structured Total Marks",
+    width: 80,
     editable: false,
   },
   {
-    field: "pm1",
-    headerName: "PM-1",
+    field: "essay",
+    headerName: "Essay Total Marks",
+    width: 80,
+    editable: false,
+  },
+  {
+    field: "q1",
+    headerName: "1",
     width: 90,
     editable: false,
   },
   {
-    field: "pm2",
-    headerName: "PM-2",
+    field: "q2",
+    headerName: "2",
     width: 90,
     editable: false,
   },
   {
-    field: "pm3",
-    headerName: "PM-3",
+    field: "q3",
+    headerName: "3",
     width: 90,
     editable: false,
   },
   {
-    field: "pm4",
-    headerName: "PM-4",
+    field: "q4",
+    headerName: "4",
     width: 90,
     editable: false,
   },
   {
-    field: "pm5",
-    headerName: "PM-5",
+    field: "q5",
+    headerName: "5",
     width: 90,
     editable: false,
   },
   {
-    field: "pm6",
-    headerName: "PM-6",
+    field: "q6",
+    headerName: "6",
     width: 90,
     editable: false,
   },
   {
-    field: "pm7",
-    headerName: "PM-7",
+    field: "q7",
+    headerName: "7",
     width: 90,
     editable: false,
   },
   {
-    field: "pm8",
-    headerName: "PM-8",
+    field: "q8",
+    headerName: "8",
     width: 90,
     editable: false,
   },
   {
-    field: "pm9",
-    headerName: "PM-9",
+    field: "q9",
+    headerName: "9",
     width: 90,
     editable: false,
   },
   {
-    field: "pm10",
-    headerName: "PM-10",
+    field: "q10",
+    headerName: "10",
     width: 90,
     editable: false,
   },
   {
-    field: "am1",
-    headerName: "AM-1",
+    field: "q11",
+    headerName: "11",
     width: 90,
     editable: false,
   },
   {
-    field: "am2",
-    headerName: "AM-2",
+    field: "q12",
+    headerName: "12",
     width: 90,
     editable: false,
   },
   {
-    field: "am3",
-    headerName: "AM-3",
+    field: "q13",
+    headerName: "13",
     width: 90,
     editable: false,
   },
   {
-    field: "am4",
-    headerName: "AM-4",
+    field: "q14",
+    headerName: "14",
     width: 90,
     editable: false,
   },
   {
-    field: "am5",
-    headerName: "AM-5",
+    field: "q15",
+    headerName: "15",
     width: 90,
     editable: false,
   },
   {
-    field: "am6",
-    headerName: "AM-6",
+    field: "q16",
+    headerName: "16",
     width: 90,
     editable: false,
   },
   {
-    field: "am7",
-    headerName: "AM-7",
-    width: 90,
-    editable: false,
-  },
-  {
-    field: "am8",
-    headerName: "AM-8",
-    width: 90,
-    editable: false,
-  },
-  {
-    field: "am9",
-    headerName: "AM-9",
-    width: 90,
-    editable: false,
-  },
-  {
-    field: "am10",
-    headerName: "AM-10",
+    field: "q17",
+    headerName: "17",
     width: 90,
     editable: false,
   },
 ];
 
-const TABLE_COLUMNS = { nonCombinedMathematics, combinedMathematics };
+const TABLE_COLUMNS = { nonMathematics, mathematics };
 
 export default TABLE_COLUMNS;
